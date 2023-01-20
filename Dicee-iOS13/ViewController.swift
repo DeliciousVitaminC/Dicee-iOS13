c47388524fca03e130e563f9848c2ace31912ed2
zhaohe <a1320555916@outlook.com>
2023-01-20 12:57:29 -0400

    first iteration

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    let UIImageArray = [UIImage(imageLiteralResourceName: "DiceOne"),UIImage(imageLiteralResourceName: "DiceTwo"),UIImage(imageLiteralResourceName: "DiceThree"),UIImage(imageLiteralResourceName: "DiceFour"),UIImage(imageLiteralResourceName: "DiceFive"),UIImage(imageLiteralResourceName: "DiceSix")]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//
+//        diceImageViewOne.image = UIImageArray[3]
+//
+//        diceImageViewOne.alpha = 0.5
     }
-
-
+    
+    
+    
+    @IBAction func rollButton(_ sender: UIButton) {
+        
+        
+        //notice here how to use 2 different way to randomize
+        diceImageViewTwo.image = UIImageArray[Int.random(in: 0...5)]
+        
+        diceImageViewOne.image = UIImageArray.randomElement()
+    }
 }
-
